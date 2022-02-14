6af87d01c468dcd1d9e6e34789c1d909d2772d76
AlexAaronWeber <a2weber94@gmail.com>
2022-02-14 09:08:09 -0500

    initial commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace CircleObjects
+{
+	public class Circle
+	{
+		//properties
+		public double Radius { get; set; }
+		
+		//constructor
+		public Circle (double radius)
+        {
+			Radius = radius;
+        }
+
+        //--------Methods-----------
+        public double CalculateCircumference()
+        {
+            double circumference = (2 * Math.PI) * Radius;
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference()//takes circ and formats to 2 decimals
+        {
+			string formatCircumference = FormatNumber(CalculateCircumference());
+			return formatCircumference;
+        }
+
+		public double CalculateArea()
+		{
+			double area = Math.PI * (Radius * Radius);
+			return area;
+		}
+
+		public string CalculateFormattedArea()//takes area and formats to 2 decimals
+		{
+			string formatArea = FormatNumber(CalculateArea());
+			return formatArea;
+		}
+
+		private string FormatNumber(double x)//round number to two decimal points
+        {
+			string newString = Math.Round(x ,2).ToString();
+			return newString;
+        }
+	}
+}
+
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,58 @@
+﻿using CircleObjects;
+
+Console.WriteLine("Welcome to the Circle Builder");
+
+bool looping = true;
+while (looping)
+{
+    double input = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write("Enter a radius: ");
+            input = double.Parse(Console.ReadLine());
+            break;
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    Circle newCircle = new Circle(input);
+
+    string circumference = newCircle.CalculateFormattedCircumference();
+    string area = newCircle.CalculateFormattedArea();
+
+    Console.WriteLine($"Circumference: {circumference}");
+    Console.WriteLine($"Area: {area}");
+
+    while (true)
+    {
+        Console.Write("Continue? (y/n): ");
+        string loopchoice = Console.ReadLine().ToLower().Trim();
+        Console.WriteLine();
+
+        if (loopchoice == "y" || loopchoice == "yes")
+        {
+            looping = true;
+            break;
+        }
+        else if (loopchoice == "n" || loopchoice == "no")
+        {
+
+            looping = false;
+            Console.WriteLine("Have fun being square, because you're no longer a-round!");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please try again");
+        }
+    }
+}
+
+
+
+
